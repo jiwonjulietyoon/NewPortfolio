22d3685aa97857ca43693c0f510785637039d4b2
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-08-04 20:48:40 +0900

    Add cloud and sun animation

--- src/components/bookPage/castle/Celestial.vue
+++ src/components/bookPage/castle/Celestial.vue
@@ -1,7 +1,54 @@
 <template>
   <section class="celestialSection">
 
-    <h2></h2>
+    <!-- CLOUDS -->
+    <div class="cloudRow c1">
+      <div class="row">
+        <div class="r1">
+          <div class="cloud"></div>
+        </div>
+        <div class="r2">
+          <div class="cloud"></div>
+        </div>
+      </div>
+    </div>
+    <div class="cloudRow c2">
+      <div class="row">
+        <div class="r1">
+          <div class="cloud"></div>
+        </div>
+        <div class="r2">
+          <div class="cloud"></div>
+        </div>
+      </div>
+    </div>
+    <div class="cloudRow c3">
+      <div class="row">
+        <div class="r1">
+          <div class="cloud"></div>
+        </div>
+        <div class="r2">
+          <div class="cloud"></div>
+        </div>
+      </div>
+    </div>
+    <div class="cloudRow c4">
+      <div class="row">
+        <div class="r1">
+          <div class="cloud"></div>
+        </div>
+        <div class="r2">
+          <div class="cloud"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- SUN -->
+    <div class="sunOuter">
+      <div class="sun"></div>
+    </div>
+
+
   </section>
 </template>
 
