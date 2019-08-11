21e8f67857bcbf9c3a34220e5feb0faa6331d3c2
Jiwon Juliet Yoon <jiwonjulietyoon@gmail.com>
2019-08-11 23:59:54 +0900

    Add jack of spade card design

--- src/components/bookPage/contactCards/Intro.vue
+++ src/components/bookPage/contactCards/Intro.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="selectedCard">
-    <div class="grungeBorder">
-      <div class="top">
-        <div class="text">Jiwon</div>
-      </div>
-      <div class="middle">
-        
-      </div>
-      <div class="bottom">
-        <div class="text">Yoon</div>
-      </div>
+    <div class="side left">
+      <div class="jack">J</div>
+      <img class="spade" src="~@/assets/spade.svg">
+    </div>
+    <div class="blackBorder">
+
+    </div>
+    <div class="side right">
+      <div class="jack">J</div>
+      <img class="spade" src="~@/assets/spade.svg">
     </div>
   </div>
 </template>
@@ -24,5 +24,37 @@ export default {
 <style lang="scss" scoped>
 @import "@/css/style.scss";
 @import "@/css/contactCards.scss";
+
+.blackBorder {
+  width: 72%;
+  height: 82%;
+  @include centerItem;
+  border: 2px solid #181818;
+  border-radius: 2px;
+}
+.side {
+  width: 14%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  padding: 3% 0;
+  &.left {
+    left: 0;
+  }
+  &.right {
+    right: 0;
+    transform: rotate(180deg);
+  }
+  & > .jack {
+    width: 100%;
+    height: auto;
+    text-align: center;
+    font-family: $fontFancy;
+    font-size: 2.5em;
+  }
+  & > .spade {
+    transform: scaleX(0.75);
+  }
+}
 
 </style>
